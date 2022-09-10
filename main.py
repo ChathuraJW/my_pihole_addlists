8a8871c0f93d87eb390885bc84582da426f3f140
ASUS-PC <wachathuraj@gmail.com>
2022-09-10 09:50:20 +0530

    Updated the scraping code according to the firebog list changes.

--- main.py
+++ main.py
@@ -1,101 +1,78 @@
-import time
+import logging
 
 import pandas
 import requests
-import wget
 
-operation_levels = ["tick", "std"]
-categories = ["suspicious", "advertising", "tracking", "malicious", "other"]
-downloaded_file_names = []
-addlist_file_names = []
+operated_lists = []
+failed_lists = []
 
 
 def main():
     # download url list
-    firebog_url = "https://v.firebog.net/hosts/csv.txt"
+    firebog_url = "https://v.firebog.net/hosts/lists.php?type=all"
     list_file_content = requests.get(firebog_url)
     open("downloads/url_list.csv", "wb").write(list_file_content.content)
-    print("firebog list downloaded.")
+    logging.info("firebog list downloaded.")
+
     # save content to numpy array
     url_list = pandas.read_csv("downloads/url_list.csv").to_numpy()
 
-    # create empty file for final lists
-    for ol in operation_levels:
-        for category in categories:
-            # create file name and add to list
-            file_name = "addlists/" + ol + "_" + category + "_addlist.txt"
-            addlist_file_names.append(file_name)
-            # create top comment of the file
-            content = "########################################################################## \n" \
-                      "# This add list created based on the lists at https://firebog.net/.\n" \
-                      "# This aggregated list is licence under the Apache-2.0 license.\n" \
-                      "# The content of this list categorized as \n" \
-                      "# ==> " + category + "\n" \
-                      "# ==> " + ol + "\n" \
-                      "########################################################################## \n"
+    # create an empty file for final lists
+    file_name = "addlists/complete_addlist.txt"
 
-            # create the file
-            file = open(file_name, "w")
-            file.write(content)
-            file.close()
-    print("Addlist file created.")
+    # create top comment of the file
+    content = "########################################################################## \n" \
+              "# This add list created based on the lists at https://firebog.net/.\n" \
+              "# This aggregated list is licence under the Apache-2.0 license.\n" \
+              "# The content of this list categorized as \n" \
+              "########################################################################## \n"
+
+    # create the file
+    file = open(file_name, "w")
+    file.write(content)
+    logging.info("Addlist file created.")
 
     # download files read content and write to final list
+    all_link_list = set()
+
     for list_entry in url_list:
-        operation_level = list_entry[1]
-        category = list_entry[0]
-        tag = "[OL: " + operation_level + "; CaT: " + category + "]"
+        link_to_list = list_entry[0]
 
-        file_name_to_write = "addlists/" + operation_level + "_" + category + "_addlist.txt"
-
-        # open file to write
-        destination_file = open(file_name_to_write, "ab")
-
-        # create list meta and add to file
-        list_meta = "\n########################################################################## \n" \
-                    "# ==> From:            " + list_entry[2] + "\n" \
-                    "# ==> Comment:         " + list_entry[3] + "\n" \
-                    "# ==> Source File URL: " + list_entry[4] + "\n" \
-                    "########################################################################## \n"
-
-        # create filename for download the addlist
-        download_file_name = "downloads/" + str(int(round(time.time() * 1000))) + "_" + \
-                             operation_level + "_" + category + "_list.temp"
-
+        # processing the links
         try:
             # download addlist
-            wget.download(list_entry[4], download_file_name)
-            downloaded_file_names.append(download_file_name)
-
-            # convert meta string to bytes format and write to the file
-            destination_file.write(bytes(list_meta, 'utf-8'))
-
-            # open the downloaded file and append to list
-            downloaded_file = open(download_file_name, "r")
-            file_content = downloaded_file.read()
-
-            # ignore empty files
-            if file_content.lower().find("service suspended") == -1:
-                destination_file.write(bytes(file_content, 'utf-8'))
-                print(list_entry[4], " ===> Added to the list. ", tag)
+            file_content = requests.get(link_to_list).content
+            logging.info("Downloaded: " + link_to_list)
+            if len(file_content) != 0:
+                # do the work
+                for link in file_content.decode("utf-8").splitlines():
+                    all_link_list.add(link)
+                # update the processed lists
+                operated_lists.append(link_to_list)
             else:
-                print(list_entry[4], " ===x Not added to the list.(empty file) ", tag)
+                logging.warning("Resource is empty: " + link_to_list)
+                # update the failed lists
+                failed_lists.append(link_to_list)
 
-            # close opened stream
-            downloaded_file.close()
+        except requests.RequestException as e:
+            logging.warning("Can not process ", link_to_list)
+            logging.warning(e)
+            # update the failed lists
+            failed_lists.append(link_to_list)
 
-        except:
-            # append error message
-            downloaded_file_names.append("N/A " + download_file_name)
-            print(list_entry[4], " ===x Not added to the list.(list download issue) ", tag)
+    logging.info("Data acquisition completed.")
 
-        # close opened stream
-        destination_file.close()
+    # write the content to file
+    for link in all_link_list:
+        file.write(link + "\n")
+    file.close()
 
-    print("Operation completed.")
+    logging.info("Addlist file content written.")
+
+    logging.info("Successes lists: " + str(operated_lists))
+    logging.info("Failed lists: " + str(failed_lists) if len(failed_lists) != 0 else "No failed lists.")
 
 
 if __name__ == "__main__":
     main()
-    # TODO clean downloaded file
-    # TODO create log file
+    # TODO try tree implementation if possible to analyze the urls
